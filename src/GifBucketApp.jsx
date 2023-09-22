a20d858ebd7c984cbe8f3f3e60ed7b816d84ec23
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-09-21 19:40:47 -0700

    add styles

--- src/GifBucketApp.jsx
+++ src/GifBucketApp.jsx
@@ -2,7 +2,11 @@
 export const GifBucketApp = ()=> {
     return (
         <>
+        {/* Titulo */}
         <h1>GifBucketApp</h1>
+        {/* Input */}
+        {/* Listado de Gif */}
+            {/* Gif Item */}
         </>
     )
 }
