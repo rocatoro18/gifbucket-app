383b8221c7da9759b3806b8a649748098a8f2bfd
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-09-22 23:04:31 -0700

    add category component and categories useState

--- src/GifBucketApp.jsx
+++ src/GifBucketApp.jsx
@@ -1,11 +1,36 @@
+import {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifBucketApp = ()=> {
+
+    // ESPACIO EN MEMORIA PARA MANEJAR CATEGORIAS
+    const [categories, setCategories] = useState(['Starfield','Halo']);
+
+    const onAddCategory = () => {
+        //
+        console.log('Hola add category');
+        // EVITAR USAR EL PUSH PARA NO MUTAR EL ESTADO
+        // EVITAR ESTO setCategories((c)=> c.push('Microsoft Flight 2020'));
+        // ...
+        // ... operador spread
+        setCategories([...categories, 'Microsoft Flight Simulator 2020']);
+        // setCategories(cat => [...cat, 'Cities Skylines 2' ]);
+    }
+
     return (
         <>
         {/* Titulo */}
         <h1>GifBucketApp</h1>
         {/* Input */}
+        <AddCategory/>
         {/* Listado de Gif */}
+        <button onClick={onAddCategory}>Agregar</button>
+        <ol>
+            {categories.map(category => {
+                return <li key={category}>{category}</li>
+            })}
+            
+        </ol>
             {/* Gif Item */}
         </>
     )
