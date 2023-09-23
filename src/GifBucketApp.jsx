ba5fda46a864ed288d993b393c4cfe9ca0345027
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-09-23 13:26:01 -0700

    add category component

--- src/GifBucketApp.jsx
+++ src/GifBucketApp.jsx
@@ -6,14 +6,16 @@ export const GifBucketApp = ()=> {
     // ESPACIO EN MEMORIA PARA MANEJAR CATEGORIAS
     const [categories, setCategories] = useState(['Starfield','Halo']);
 
-    const onAddCategory = () => {
+    const onAddCategory = (newCategory) => {
+        // Verificar que no se inserte una categoria existente
+        if(categories.includes(newCategory)) return;
         //
-        console.log('Hola add category');
+        //console.log(newCategory);
         // EVITAR USAR EL PUSH PARA NO MUTAR EL ESTADO
         // EVITAR ESTO setCategories((c)=> c.push('Microsoft Flight 2020'));
         // ...
         // ... operador spread
-        setCategories([...categories, 'Microsoft Flight Simulator 2020']);
+        setCategories([newCategory,...categories]);
         // setCategories(cat => [...cat, 'Cities Skylines 2' ]);
     }
 
@@ -22,9 +24,12 @@ export const GifBucketApp = ()=> {
         {/* Titulo */}
         <h1>GifBucketApp</h1>
         {/* Input */}
-        <AddCategory/>
+        <AddCategory 
+        //setCategories={setCategories}
+        onNewCategory = {event => onAddCategory(event)}
+        />
         {/* Listado de Gif */}
-        <button onClick={onAddCategory}>Agregar</button>
+        {/*<button onClick={onAddCategory}>Agregar</button>*/}
         <ol>
             {categories.map(category => {
                 return <li key={category}>{category}</li>
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const AddCategory = () => {
+export const AddCategory = ({onNewCategory}) => {
 
     const [inputValue,setInputValue] = useState('Starfield');
 
@@ -10,11 +10,14 @@ export const AddCategory = () => {
 
     const onSubmit = (event) => {
         event.preventDefault();
-        console.log(inputValue);
+        if(inputValue.trim().length <= 1) return;
+        //setCategories(categories => [inputValue,...categories]);
+        onNewCategory(inputValue.trim());
+        setInputValue('');
     }
 
     return (
-        <form onSubmit={(event) => onSubmit(event)}>
+        <form onSubmit={onSubmit}>
             <input
                 type="text"
                 placeholder="Buscar gifs"
